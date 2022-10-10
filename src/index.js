0ef3b827b49d198b355bce57e1b9ca39d7043e39
shanzventures <shanmcgov93@gmail.com>
2022-10-10 09:11:07 +0100

    Edited weekly forecast styling

--- src/index.js
+++ src/index.js
@@ -68,7 +68,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "866a208a73eeff02182218e9441647a1";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
